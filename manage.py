3899cdad102369c8d574a283ac65b3809578bca6
Wesley Paulo Gonçalves <wpgoncalves1984@gmail.com>
2022-09-27 22:11:43 -0300

    Alteração de Procfile e manage.py.

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digital_showcase.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digital_showcase.settings')  # noqa: E501
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
